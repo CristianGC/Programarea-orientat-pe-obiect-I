6fe9313b552e43b9f462b29a8881e61252d439ba
CristianGC <cristian.ghinea.1996@gmail.com>
2021-03-28 22:59:13 +0300

    Structura primitivă a indexului

--- index.php
+++ index.php
@@ -1,14 +1,38 @@
 <?php
 
-  // Test
-  if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+  if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) 
+  {
     $uri = 'https://';
-  } else {
+  } 
+  else
+  {
     $uri = 'http://';
   }
+  
   $uri .= $_SERVER['HTTP_HOST'];
-  header('Location: '.$uri.'/dashboard/');
+  header('Location: '.$uri.'/dashboard/');  
   exit;
+
 ?>
 Something is wrong with the XAMPP installation :-(
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+      <header>
+        <nav>
+          <div><a href="">1 Calculator.</a></div>
+          <div><a href="">2 Editor de text.</a></div>
+          <div><a href="">3 Editor de imagine.</a></div>
+          <div><a href="">4 Mine jocă.</a></div>
+        </nav>
+      </header>
+  </body>
+</html>
 
