af14d442bfba94a916f0d3db5535c423cf63efb5
CristianGC <cristian.ghinea.1996@gmail.com>
2021-03-29 21:18:04 +0300

    Crearea o clasa pentru: Instalarea URL-ului http și https.

--- index/View/html/body/header/nav/Customization/Customization.php
+++ index/View/html/body/header/nav/Customization/Customization.php
@@ -1,15 +1,57 @@
 <?php 
 
-    if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) 
+//Instalarea URL-ului http și https
+class Installing_the_http_and_https_url
+{
+    protected static $_instance;
+    private static $_url;
+
+    public function get_http_or_https()
     {
-    $uri = 'https://';
-    } 
-    else
-    {
-    $uri = 'http://';
+        if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) 
+        {
+            self::$_url = 'https://';
+        } 
+        else
+        {
+            self::$_url = 'http://';
+        }
+    
+        return self::$_url .= $_SERVER['HTTP_HOST'];
     }
 
-    $uri .= $_SERVER['HTTP_HOST'];
+    public static function getInstaence()
+    {
+        if (self::$_instance == null) 
+        {
+            self::$_instance = new self;
+        }
+
+        self::$_url = null;
+
+        return self::$_instance;
+    } 
+
+    private function __construct() 
+    {
+
+    }
+
+    private function __clone()
+    {
+        
+    }
+
+    public function __wakeup()
+    {
+        
+    }
+
+}
+
+echo Installing_the_http_and_https_url::getInstaence()->get_http_or_https();
 
 ?>
+
+
 
