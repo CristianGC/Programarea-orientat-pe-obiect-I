48f315ea9808587120346b12b2f805ad544847c4
CristianGC <cristian.ghinea.1996@gmail.com>
2021-03-30 19:38:06 +0300

    Mai întâi, comentează calculator pentru a vedea ce articole am nevoie.

--- Calculator/index/JavaScript/index.js
+++ Calculator/index/JavaScript/index.js
@@ -26,3 +26,34 @@ Object.assign(User.prototype, Voce);;
 
 new User('Cristian').Salut();
 
+class Calculator
+{
+    /*
+        0 Crearea tuturor elementelor dinamic.
+        1 Titlu.
+        2 Butonul minus.
+        3 Butonul maxim.
+        4 Butonul exit.
+        5 Afișăm ce sa scris și rezultatul. În format șir.
+        7 Butonul CE
+        7 Butonul C
+        7 Butonul Scoateți elementul de la capătul liniei..
+        6 Butonul %
+        7 Butonul /
+        7 Butonul *
+        7 Butonul -
+        7 Butonul +
+        7 Butonul =
+        7 Butoanele 0 1 2 3 4 5 6 7 8 9.
+        7 Butonul ,
+        7 Butonul pentru a schimba numărul în pozitiv sau negativ.
+        7 Butonul pentru a schimba numărul în pozitiv sau negativ.
+
+    */
+
+    constructor()
+    {
+
+    }
+}
+
